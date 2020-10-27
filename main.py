db708d4e39e877eb28a34e500425f8ebf4610bd8
U Sai Ko <sai1997ko@gmail.com>
2020-10-27 11:24:32 +0630

    fix

--- main.py
+++ main.py
@@ -48,5 +48,5 @@ def index():
     else:
         return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
