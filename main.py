a5a2e98162cdc150b37e7afc1e697130de3d3f6c
U Sai Ko <sai1997ko@gmail.com>
2020-10-27 10:58:44 +0630

    fix

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import pytesseract as tess
 from PIL import Image
 import os
 
-from werkzeug.utils import secure_filename
-UPLOAD_FOLDER = "images"
+# from werkzeug.utils import secure_filename
+# UPLOAD_FOLDER = "images"
+
+
 ALLOWED_EXTENSIONS = set(["txt", "pdf", "png", "jpg", "jpeg", "gif"])
 
 app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+# app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-app.secret_key = 'saiko192168'
+app.secret_key = 'secrectkey'
 
 
 
@@ -33,18 +35,18 @@ def index():
             flash("No selected file")
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-            uploadimg = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-            img = Image.open(uploadimg)
+            # filename = secure_filename(file.filename)
+            # file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            imagefile = request.files.get('file', '') 
+            # uploadimg = imagefile
+            img = Image.open(imagefile)
             text = tess.image_to_string(img)
-            if text != None :
-                os.remove(uploadimg)
+            # if text != None :
+            #     os.remove(uploadimg)
             
             return render_template("index.html",text=text)
     else:
         return render_template("index.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
